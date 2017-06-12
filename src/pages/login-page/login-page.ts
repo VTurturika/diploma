40deac2538006fe5deec9fa8eed6a809169ac99e
Victor Turturika <v.turturika@gmail.com>
2017-06-12 20:27:49 +0300

    feat(auth): add loginProvider, check login/password on server

--- src/pages/login-page/login-page.ts
+++ src/pages/login-page/login-page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {NavController, NavParams, AlertController} from 'ionic-angular';
+import {NavController, NavParams, AlertController, LoadingController} from 'ionic-angular';
 import {ViewReceiptPage} from "../view-receipt-page/view-receipt-page";
 import {TabsPage} from "../tabs/tabs";
+import {LoginProvider} from "../../providers/loginProvider";
 
 /**
  * Generated class for the LoginPage page.
@@ -13,12 +14,19 @@ import {TabsPage} from "../tabs/tabs";
 @Component({
   selector: 'page-login-page',
   templateUrl: 'login-page.html',
+  providers: [LoginProvider]
 })
 export class LoginPage {
   data : any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
-    this.data = {username: "", password: ""}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public alertCtrl: AlertController,
+    public loginProvider: LoginProvider,
+    public loadingCtrl: LoadingController,
+  ) {
+    this.data = {login: "", password: ""}
   }
 
   ionViewDidLoad() {
@@ -26,19 +34,28 @@ export class LoginPage {
   }
 
   public login() {
-    console.log(this.data);
-    if(this.data.username == "admin" && this.data.password == "123") {
-      console.log("login");
-      this.navCtrl.push(TabsPage);
-    } else {
-      let alert = this.alertCtrl.create({
-        title: 'Error',
-        subTitle: 'Wrong username or password.\nTry again.',
-        buttons: ['OK']
-      });
 
-      alert.present();
-    }
+    let loader = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    loader.present();
+    this.loginProvider.login(this.data)
+      .then(res => {
+
+        loader.dismissAll();  
+        let result:any = res
+
+        if(result.isLogined) {
+          this.navCtrl.push(TabsPage);
+        }
+        else {
+          let alert = this.alertCtrl.create({
+            title: 'Error',
+            subTitle: 'Wrong username or password.\nTry again.',
+            buttons: ['OK']
+          });
+          alert.present();
+        }
+      })
   }
-
 }
---
+++ src/providers/loginProvider.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import {Http, Headers} from "@angular/http";
+
+@Injectable()
+export class LoginProvider {
+
+  endpoint: String = "http://54.197.180.84/api";
+
+  constructor(
+    public http: Http
+  ) {
+    console.log('Hello UserProvider Provider');
+  }
+
+  login(credentials: any) {
+
+    console.log('login start');
+    console.log(credentials);
+    return new Promise(resolve => {
+      const url = `${this.endpoint}/user/login`;
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      this.http.post(url, credentials, {headers: headers})
+        .map(res => res.json())
+        .subscribe(response => {
+          console.log('login request success');
+          console.log('Response: ' + JSON.stringify(response));
+
+          if(response.userToken) resolve({
+            isLogined: true,
+            token: response.userToken
+          })
+          else resolve({
+            isLogined: false
+          })
+        })
+    })
+  }
+
+}
